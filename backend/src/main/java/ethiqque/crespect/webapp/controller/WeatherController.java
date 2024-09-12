bb60ddef48767571ca18a046a73950f8d842fd7e
Daniel Krasnov <danielpyld.work@gmail.com>
2024-09-12 07:30:29 +0300

    refactored controller

--- backend/src/main/java/ethiqque/crespect/webapp/controller/WeatherController.java
+++ backend/src/main/java/ethiqque/crespect/webapp/controller/WeatherController.java
@@ -7,12 +7,11 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/weather")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = {"http://localhost:3001", "http://localhost:3000"})
 public class WeatherController {
 
     @Autowired
